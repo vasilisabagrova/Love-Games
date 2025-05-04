c8114a7fcdc037dd5a45b83aba9bce977b17641e
vasilisabagrova <lovegames.vasilisa@gmail.com>
2025-05-04 16:56:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,25 @@ const dropdown = document.querySelector('.dropdown');
 let isDropdownOpen = false;
 const defaultText = "Общий каталог";
 const openText = "Подборки";
+const dropdownContent = document.querySelector('.dropdown-content');
+const dropdownHeight = dropdownContent.scrollHeight; // Получаем полную высоту
 
 // Функция для открытия/закрытия дропдауна
 function toggleDropdown(event) {
     isDropdownOpen = !isDropdownOpen;
-    const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.style.display = isDropdownOpen ? 'block' : 'none';
-    catalogButton.textContent = isDropdownOpen ? openText : defaultText;
 
-    // Предотвращаем переход по ссылке, если это первый клик
     if (isDropdownOpen) {
-        event.preventDefault();
+        dropdownContent.style.display = 'block'; // Показываем перед анимацией
+        dropdownContent.style.maxHeight = dropdownHeight + 'px'; // Анимируем высоту
+        catalogButton.textContent = openText;
+        event.preventDefault(); // Предотвращаем переход по ссылке
+    } else {
+        dropdownContent.style.maxHeight = '0'; // Анимируем высоту обратно
+        catalogButton.textContent = defaultText;
+        // Задержка перед скрытием, чтобы анимация успела завершиться
+        setTimeout(() => {
+            dropdownContent.style.display = 'none';
+        }, 300); // Время задержки должно соответствовать времени анимации
     }
 }
 
@@ -25,14 +33,16 @@ catalogButton.addEventListener('click', (event) => {
 // Обработчик наведения мыши для устройств с поддержкой hover
 if (window.matchMedia("(hover: hover)").matches) {
     dropdown.addEventListener('mouseenter', () => {
-        const dropdownContent = document.querySelector('.dropdown-content');
         dropdownContent.style.display = 'block';
+        dropdownContent.style.maxHeight = dropdownHeight + 'px';
         catalogButton.textContent = openText;
     });
 
     dropdown.addEventListener('mouseleave', () => {
-        const dropdownContent = document.querySelector('.dropdown-content');
-        dropdownContent.style.display = 'none';
+        dropdownContent.style.maxHeight = '0';
         catalogButton.textContent = defaultText;
+        setTimeout(() => {
+            dropdownContent.style.display = 'none';
+        }, 300);
     });
 }
